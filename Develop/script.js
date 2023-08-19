6fd4357bc3fd6f85aeb64c0d8a129216683855a1
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-08-19 15:16:34 -0400

    Basic layout and functionality

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,9 @@
 //TODO: The dayjs time needs to be formatted
 setInterval(function () 
   {
-    const time = dayjs();
-    document.getElementById("timeEl").innerHTML = time;
+    const time = dayjs().format('DD/MM/YYYY');
+    document.getElementById("timeEl").innerHTML = "Current time: " + time;
 
 }, 1000);
 
+var input = document.getElementsByClassName("textfield");
