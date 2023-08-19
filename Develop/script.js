aee6c7f967b38832f088df878a1b2098706ffcb4
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-08-19 18:00:33 -0400

    Still working on functionality of localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -7,31 +7,3 @@ setInterval(function ()
 
 }, 1000);
 
-function toggleLocalStorage() {
-    const textArea = document.getElementById('textArea');
-    const toggleButton = document.getElementById('toggleButton');
-  
-    // Check if there's already saved content
-    const savedValue = localStorage.getItem('savedText');
-  
-    if (savedValue) {
-      // If saved content exists, delete it from localStorage and clear the textarea
-      localStorage.removeItem('savedText');
-      textArea.value = '';
-      toggleButton.textContent = '🔓'; // Unlocked icon
-    } else {
-      // If no saved content, save the textarea content to localStorage
-      localStorage.setItem('savedText', textArea.value);
-      toggleButton.textContent = '🔒'; // Locked icon
-    }
-  }
-  
-  // When the page loads, retrieve the saved value from localStorage and display it in the textarea
-  window.onload = function() {
-    const savedValue = localStorage.getItem('savedText');
-    if (savedValue) {
-      document.getElementById('textArea').value = savedValue;
-      document.getElementById('toggleButton').textContent = '🔒'; // Locked icon
-    }
-  };
-  
